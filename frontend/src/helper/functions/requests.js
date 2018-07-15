9c4037f9b4cb7fdb9b606743a00124950a04be23
ali <aliasgary73@gmail.com>
2018-07-15 18:13:50 +0430

    change localIP to localhost

--- frontend/src/helper/functions/requests.js
+++ frontend/src/helper/functions/requests.js
@@ -3,25 +3,25 @@ import request from 'superagent'
 export default {
   addUser (username, wisId) {
     return request
-      .post('http://192.168.43.125:3000/users/add')
+      .post('http://localhost:3000/users/add')
       .send({ username: username, wisId: wisId })
       .then(res => res)
   },
   fetchUsers (wisId) {
     return request
-      .get('http://192.168.43.125:3000/users/fetch')
-      .query({ wisId: "0" })
+      .get('http://localhost:3000/users/fetch')
+      .query({ wisId })
       .then(res => res.body)
   },
   fetchGraph (wisId) {
     return request
-      .get('http://192.168.43.125:3000/graph/fetch')
+      .get('http://localhost:3000/graph/fetch')
       .query({ wisId: "0" })
       .then(res => res.body)
   },
   addTrans (transObj) {
     return request
-      .post('http://192.168.43.125:3000/transaction/add')
+      .post('http://localhost:3000/transaction/add')
       .send(transObj)
       .then(res => res)
   },
