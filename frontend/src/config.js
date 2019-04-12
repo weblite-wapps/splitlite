7a1705e6b29b56b13643ef641c0df48fec0a53f6
Masoud Salehi <msk.masoud.74@gmail.com>
2019-04-12 18:28:20 +0430

    chore: Change port; Remove SSL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 export default process.env.NODE_ENV === 'development' ? {
-  server: 'https://localhost:3092',
+  server: 'https://localhost:4150',
 } : {
-  server: 'https://weblite.me:3092',
+  server: 'https://wapp.weblite.me/splitlite',
 }
